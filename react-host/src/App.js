34fcbf4e7358ce64d16a9cf2e9141ca7384402e4
gnganapath <gnganapathi@gmail.com>
2023-02-06 22:23:44 +0530

    first commit=> Created host , child react app using npx cra

---
+++ react-host/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          React Host app
+        </p>
+      
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-remote/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">      
+        <p>
+          React child app - Remote app
+        </p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
